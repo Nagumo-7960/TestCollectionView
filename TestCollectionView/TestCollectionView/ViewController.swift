7875c78db3bd0e826a266bcf3367b467d9c7b219
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-23 16:35:08 +0900

    collection viewの表示に成功

--- TestCollectionView/TestCollectionView/ViewController.swift
+++ TestCollectionView/TestCollectionView/ViewController.swift
@@ -7,23 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate,
-UICollectionViewDataSource
-{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
-    
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+}
 
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .blue
+        
+        return cell
+    }
 }
 
