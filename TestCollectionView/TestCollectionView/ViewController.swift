23530beffc3022ca2bf200b826add7074ae346f5
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-23 15:13:19 +0900

    initial commit

---
+++ TestCollectionView/TestCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCollectionView
+//
+//  Created by なぐも on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
