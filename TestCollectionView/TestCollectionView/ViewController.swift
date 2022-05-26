76ba3adfc94f33ef33c213220adbebb55d8d35db
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-26 23:21:46 +0900

    cellのコンテンツを角丸に変更

--- TestCollectionView/TestCollectionView/ViewController.swift
+++ TestCollectionView/TestCollectionView/ViewController.swift
@@ -8,17 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let layout = UICollectionViewFlowLayout()
+            layout.itemSize = CGSize(width: 170, height: 170)
+            layout.minimumInteritemSpacing = 10
+            layout.minimumLineSpacing = 50
+            collectionView.collectionViewLayout = layout
     }
-
-    
+        
 }
 
 
+
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
@@ -30,7 +37,8 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         cell.backgroundColor = .blue
-        
+        cell.layer.cornerRadius = 12
+
         return cell
     }
 }
