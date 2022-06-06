512c0713c084a097358da4b5935fde8347a23860
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-06-06 16:50:04 +0900

    QiitaAPIで取得した情報をtableViewに表示

--- TestCollectionView/TestCollectionView/ViewController.swift
+++ TestCollectionView/TestCollectionView/ViewController.swift
@@ -44,16 +44,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        view.addSubview(tableView)
-//        tableView.frame.size = view.frame.size
-//
-//        tableView.delegate = self
-//        tableView.dataSource = self
-//        tableView.register(QiitaTableViewCell.self, forCellReuseIdentifier: cellId)
+        view.addSubview(tableView)
+        tableView.frame.size = view.frame.size
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(QiitaTableViewCell.self, forCellReuseIdentifier: cellId)
         navigationItem.title = "Qiitaの記事"
         navItem.title = "ああああ"
         
-//        getQiitaAPI()
+        getQiitaAPI()
     }
     
     
@@ -138,7 +138,27 @@ class QiitaTableViewCell: UITableViewCell {
         return label
     }()
     
-
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(userImageView)
+        addSubview(bodyTextLabel)
+        [
+            userImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            userImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            userImageView.widthAnchor.constraint(equalToConstant: 50),
+            userImageView.heightAnchor.constraint(equalToConstant: 50),
+            
+            bodyTextLabel.leadingAnchor.constraint(equalTo: userImageView.trailingAnchor, constant: 20),
+            bodyTextLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            ].forEach{ $0.isActive = true }
+        
+        userImageView.layer.cornerRadius = 50 / 2
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 
